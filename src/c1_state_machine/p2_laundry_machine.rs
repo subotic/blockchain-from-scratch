52d5c5af2e87bf97c2f1bd02b5e6e6b6d79d5ca0
Ivan Subotic <400790+subotic@users.noreply.github.com>
2024-05-27 06:47:41 +0200

    add p2 laundry

--- src/c1_state_machine/p2_laundry_machine.rs
+++ src/c1_state_machine/p2_laundry_machine.rs
@@ -40,7 +40,27 @@ impl StateMachine for ClothesMachine {
     type Transition = ClothesAction;
 
     fn next_state(starting_state: &ClothesState, t: &ClothesAction) -> ClothesState {
-        todo!("Exercise 3")
+        match starting_state {
+            ClothesState::Clean(life) if *life > 1 => match t {
+                ClothesAction::Wear => ClothesState::Dirty(life - 1),
+                ClothesAction::Wash => ClothesState::Wet(life - 1),
+                ClothesAction::Dry => ClothesState::Clean(life - 1),
+            },
+            ClothesState::Dirty(life) if *life > 1 => match t {
+                ClothesAction::Wear => ClothesState::Dirty(life - 1),
+                ClothesAction::Wash => ClothesState::Wet(life - 1),
+                ClothesAction::Dry => ClothesState::Dirty(life - 1),
+            },
+            ClothesState::Wet(life) if *life > 1 => match t {
+                ClothesAction::Wear => ClothesState::Dirty(life - 1),
+                ClothesAction::Wash => ClothesState::Wet(life - 1),
+                ClothesAction::Dry => ClothesState::Clean(life - 1),
+            },
+            ClothesState::Tattered => {
+                return ClothesState::Tattered;
+            },
+            _ => ClothesState::Tattered,
+        }
     }
 }
 
